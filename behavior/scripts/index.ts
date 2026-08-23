7799f2436ab8d354e435abb2baa3d2903b25ccb5
ruanhor <3915264929@qq.com>
2026-08-23 12:34:38 +0800

    refactor: define luckblock and recipes via mcx components

--- behavior/scripts/index.ts
+++ behavior/scripts/index.ts
@@ -2,6 +2,8 @@ import { createApp } from "@mbler/mcx";
 import app from "./app.mcx";
 import { world } from "@minecraft/server";
 import "./DefineItems.mcx";
+import "./components/blocks.mcx";
+import "./components/recipes.mcx";
 import { version } from "./config";
 import { registryCommand } from "@mbler/mcx";
 import { LuckBlockCore } from "./core/luckBlock";
@@ -10,5 +12,6 @@ import { giveLuckBlockCommand } from "./command/giveluckblock";
 LuckBlockCore.startLoop();
 registryCommand(debugCommand);
 registryCommand(giveLuckBlockCommand);
+// @ts-ignore
 createApp(app).mount(world);
 console.log(`LuckBlock ${version} success loaded`);
